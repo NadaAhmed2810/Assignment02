f2b7167e41c7228218b2708d95daa5acc2709ba0
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-15 12:53:21 +0200

    implicit and explicit Casting

--- ASS02/Program.cs
+++ ASS02/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Value Type Casting
+            #region implicit casting -Safe Casting
+            //int x = 4;//4 byte
+            //long y = x;
+            #endregion
+            #region Explicit Casting_UnSafe Casting _Not recommended 
+            //long X = 102020304050; //8 byte
+            //int Y;
+            //checked 
+            //{
+            //    Y = (int)X;
+            //}
+            //Console.WriteLine(Y);
+            //int X = 88;
+            //double Y = X;//Y=(double)X
+            //double X = 88.22;
+            //int Y =(int) X;
+            //Console.WriteLine(Y);
+
+            #endregion
+            #endregion
         }
     }
 }
