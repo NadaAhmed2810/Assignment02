69d5d975d5e7668d7bfc859237e48c648f99a4ec
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-18 10:55:17 +0200

    Q6

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -89,6 +89,11 @@
             //        break;
             //}
             #endregion
+            #region Q6: Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+            //Console.Write("Enter A Number:");
+            //int num=int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++) Console.WriteLine(num * i);
+            #endregion
         }
     }
 }
