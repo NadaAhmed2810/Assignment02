61286586d2ca5c358271ef6f8ee70eb634620e50
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-18 09:53:48 +0200

    Q1

---
+++ Assignment02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1: Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+        
+           //Console.Write("Enter a Number to check if can divide by 3 and 4 or not : ");
+           //int num=int.Parse(Console.ReadLine());
+           // if (num % 3 == 0 && num % 4 == 0) Console.WriteLine("Yes");
+           // else Console.WriteLine("No");
+
+            #endregion
+        }
+    }
+}
